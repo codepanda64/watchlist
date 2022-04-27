fa80a833ab72b529acc43c09e395832a9268ef16
codepanda <17920082@qq.com>
2022-04-27 22:57:11 +0800

    Organize application with package

---
+++ watchlist/errors.py
@@ -0,0 +1,18 @@
+from flask import render_template
+
+from watchlist import app
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('errors/400.html'), 400
+
+
+@app.errorhandler(404)
+def bad_request(e):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def bad_request(e):
+    return render_template('errors/500.html'), 500
